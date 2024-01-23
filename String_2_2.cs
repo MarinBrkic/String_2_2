deabfd551d05a35fbd9b05e283af34a1734f7750
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-23 12:08:07 +0100

    Add files via upload

---
+++ String_2_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NizZnakova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi neki niz: ");
+            string unos = Console.ReadLine();
+
+            if (unos.Length < 5)
+            {
+                Console.WriteLine("Ispis: " + unos);
+            }
+            else
+            {
+                Console.WriteLine("Ispis: " + unos.Substring(2, unos.Length -4));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
